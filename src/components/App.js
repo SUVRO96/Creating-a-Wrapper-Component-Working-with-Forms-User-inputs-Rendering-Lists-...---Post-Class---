8ac222e50e027329b43a515a7a578bc4551abbe3
Subhradip Nath <97626209+SUVRO96@users.noreply.github.com>
2022-05-21 23:20:25 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.relatives = [
+      { name: "Arijit Nath", relation: "father" },
+      { name: "Ananya Nath", relation: "sister" },
+      { name: "sadhana Nath", relation: "mother" },
+      { name: "Shanti Nath", relation: "grand mother" },
+      { name: "Debrani Mukherjee", relation: "father" }
+    ];
+  }
+  render() {
+    return (
+      <div id="main">
+        <ol key={"relativeList"}>
+          {this.relatives &&
+            this.relatives.map((item, index) => (
+              <li key={`relativeListItem${index + 1}`}>{item.name}</li>
+            ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
